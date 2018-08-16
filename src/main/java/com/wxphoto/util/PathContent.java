fe1993a3b770a75a8bca183389f2050261bc5d81
hanyangji <893891013@qq.com>
2018-08-16 12:44:10 +0800

    201808161244

--- src/main/java/com/wxphoto/util/PathContent.java
+++ src/main/java/com/wxphoto/util/PathContent.java
@@ -1,7 +1,8 @@
 package com.wxphoto.util;
 
 public class PathContent {
-	public static String DownLoadImg="/Users/hanyangji/Downloads/work/mcht";
+//	public static String DownLoadImg="/Users/hanyangji/Downloads/work/mcht";
+	public static String DownLoadImg="C:\\Program Files\\Apache Software Foundation\\mcht";
 	public static String ZFB="zfb";
 	public static String WX="wx";
 
